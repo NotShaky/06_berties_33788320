663ae56a7e36cb191c98437cc9677bf9f80c1b4a
NotShaky <shoaib.a.rahman@gmail.com>
2025-11-12 13:40:45 +0000

    part b

--- routes/books.js
+++ routes/books.js
@@ -11,5 +11,15 @@ router.get('/search-result', function (req, res, next) {
     res.send("You searched for: " + req.query.keyword)
 });
 
-// Export the router object so index.js can access it
+router.get('/list', function(req, res, next) {
+        let sqlquery = "SELECT * FROM books"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                next(err)
+            }
+            res.send(result)
+         });
+    });
+
 module.exports = router
