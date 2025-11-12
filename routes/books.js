d12f25b90898e086e233ad16e2379c83763701b6
NotShaky <151756800+NotShaky@users.noreply.github.com>
2025-11-12 23:49:16 +0000

    start of extention

--- routes/books.js
+++ routes/books.js
@@ -11,29 +11,41 @@ router.get('/search-result', function (req, res, next) {
     res.send("You searched for: " + req.query.keyword)
 });
 
-router.get('/list', function(req, res, next) {
-        let sqlquery = "SELECT * FROM books"; // query database to get all the books
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                next(err)
-            }
-            res.render("list.ejs", {availableBooks: result})
-         });
-    });
+router.get('/addbook', function(req, res, next) {
+    res.render("addbook.ejs")
+});
 
-router.post('/bookadded', function (req, res, next) {
-    // saving data in database
-    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
     // execute sql query
-    let newrecord = [req.body.name, req.body.price]
-    db.query(sqlquery, newrecord, (err, result) => {
+    db.query(sqlquery, (err, result) => {
         if (err) {
             return next(err)
         }
-        // render a confirmation page
-        res.render('bookadded.ejs', { addedBook: { name: req.body.name, price: req.body.price } })
-    })
-})
+        // render with the name `books` so [views/list.ejs](views/list.ejs) receives it
+        res.render("list.ejs", { books: result })
+     });
+});
+
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT id, name, price FROM books WHERE price < 20";
+    db.query(sqlquery, (err, result) => {
+        if (err) return next(err);
+        res.render('bargainbooks.ejs', { books: result });
+    });
+});
+
+router.post('/bookadded', function(req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+    let newrecord = [req.body.name, req.body.price];
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        // redirect to the list route so it re-queries the DB and shows the new book
+        res.redirect('/books/list');
+    });
+});
 
 module.exports = router
