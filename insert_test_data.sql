e0afbe8ef3b23624879ef2520480942b09c8562f
NotShaky <151756800+NotShaky@users.noreply.github.com>
2025-11-27 01:24:41 +0000

    sql prompt update

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,5 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, first, last, email, hashedPassword) VALUES ('gold', 'Gold', 'Member', 'gold@example.com', '$2b$10$fxvDw/bh7jTm73Z7dDCFDuIdsI2lGs77XrxZt/wnT4mJvMHmXkO3K');
